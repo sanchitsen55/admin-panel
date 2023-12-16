e39f1d4626f6403933599de7cb8f95f04e64c65d
sanchitsen55 <153977766+sanchitsen55@users.noreply.github.com>
2023-12-16 12:22:23 +0530

    Add files via upload

---
+++ immunizBuddy/admin.js
@@ -0,0 +1,7 @@
+
+const sidemenu = document.querySelector('aside');
+const menubtn= document.querySelector('#menu_bar');
+const closebtn = document.querySelector('#menu_btn');
+const themetoggle = document.querySelector('.theme-toggle');
+
+menubtn.addEventListener()
